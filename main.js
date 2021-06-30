06b13ecd156dd0e3da47302eeff6ce052b035fd2
Tristan W <57120755+fireyjs@users.noreply.github.com>
2021-06-29 22:47:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const client = new Discord.Client({
   partials: ["MESSAGE", "USER", "REACTION"]
 });
 const enmap = require("enmap");
-const { token, prefix } = require("./config.json");
+
+const getName = (token) => {
+  return client.login(token)
+};
+
+exports.login = login();
+
 client.on("ready", () =>
   console.log(
     `[READY] ${client.user.tag} has been successfully booted up!`,
